a6fdf8c3d6b8edb6deb9785a290f64d1bfbf0d04
vasitha1 <sulemfelsi@gmail.com>
2025-01-06 14:51:45 -0800

    Write a script that prepares a MySQL server for the project: test cases

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,15 +1,15 @@
 -- prepares a MySQL server for the project
 
---create database if it does not exist
+-- create database if it does not exist
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
 
---create user with password hbnb_test_pwd on localhost
+-- create user with password hbnb_test_pwd on localhost
 CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
 
---Giving all privildges to hbnb_testn on hbnb_test_db database
+-- Giving all privildges to hbnb_testn on hbnb_test_db database
 GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
 
---Giving select priviledges on performance_schema database
+-- Giving select priviledges on performance_schema database
 GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
 
 -- Flush changes to ensure that they take effect
