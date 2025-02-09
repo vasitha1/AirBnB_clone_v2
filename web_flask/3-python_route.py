d5861878100cb3922f60dab48cc63d92a9d011dd
vasitha1 <sulemfelsi@gmail.com>
2025-02-09 18:07:47 +0000

    Task 3: handling the option of an alternative variable passing

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""Starts a Flask web application.
+
+The application listens on 0.0.0.0, port 5000.
+Routes:
+    /: Displays 'Hello HBNB!'
+"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """Method to display Hello HBNB!"""
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """Method to display HBNB!"""
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c(text):
+    """Method to display C followed by the text parsed!"""
+    text = text.replace("_", " ")
+    return "C {}".format(text)
+
+
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python(text="is cool"):
+    """Method to display C followed by the text parsed!"""
+    text = text.replace("_", " ")
+    return "Python {}".format(text)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
