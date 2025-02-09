93a4a937f20bef3c037e67a1b6ce05fda7b59e39
vasitha1 <sulemfelsi@gmail.com>
2025-02-09 17:34:08 +0000

    Adding an /HBMB route which returns "HBMB"

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Starts a Flask web application.
+
+The application listens on 0.0.0.0, port 5000.
+Routes:
+    /: Displays 'Hello HBNB!'
+"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """Method to display Hello HBNB!"""
+    return "Hello HBNB!"
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """Method to display Hello HBNB!"""
+    return "HBNB!"
+
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
